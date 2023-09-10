a349c3371851f46e979e9c04889117343ff40b3b
Ajibade Isiaka <isiakaajibade581@gmail.com>
2023-09-10 18:38:16 +0100

    updated code with explanation 1

--- controller/homecontroller.js
+++ controller/homecontroller.js
@@ -2,8 +2,25 @@
 //@Desc: Homepage
 //@Acces: Public
 
+const Post = require("../model/post");
+
 const Home = async (req, res) => {
-  res.json({ msg: "WELCOME TO THE COMMUNITY" });
+  const posts = await Post.find()
+    .populate("author", "profile.userName")
+    .sort({ dateCreated: -1 });
+
+  const homePosts = posts.map((post) => {
+    const selectedProperties = new Post(post).toJSON();
+    selectedProperties.Likes = selectedProperties.likes.length;
+    selectedProperties.Comments = selectedProperties.comments.length;
+    selectedProperties.author = selectedProperties.author.profile;
+    delete selectedProperties.likes;
+    delete selectedProperties.comments;
+    delete selectedProperties.__v;
+    delete selectedProperties._id;
+    return selectedProperties;
+  });
+  res.json({ msg: "WELCOME TO THE COMMUNITY", homePosts });
 };
 
 module.exports = Home;
